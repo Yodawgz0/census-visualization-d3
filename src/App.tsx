66105d5c72a4280f55be27e62337ef7a78aa3fd8
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2024-02-11 19:47:30 -0500

    adding homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import "./App.scss";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <HomePage />
       </header>
     </div>
   );
