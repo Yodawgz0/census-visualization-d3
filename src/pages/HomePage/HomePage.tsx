0f353cff42d2269107e88138c506c65108d6bdc0
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2024-02-12 15:11:30 -0500

    setup of pages

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 function HomePage() {
+  const [data, setData] = useState<string>("");
+
+  useEffect(() => {
+    // Axios GET request
+    axios
+      .get("http://localhost:8080/")
+      .then((response) => {
+        // setData(response.data);
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error("Error fetching data", error);
+      });
+  }, []);
+
   return (
     <div>
       <p>HomePage</p>
+      {data}
     </div>
   );
 }
