c659641aa8b5f712d8c82c65da49a9fdacc53c93
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2024-02-13 17:30:32 -0500

    testing data and addign type

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,17 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import { censusData } from "./IProps";
 
 function HomePage() {
-  const [data, setData] = useState<string>("");
+  const [data, setData] = useState<censusData[]>([]);
 
   useEffect(() => {
     // Axios GET request
     axios
       .get("http://localhost:8080/")
       .then((response) => {
-        // setData(response.data);
         console.log(response);
+        setData(response.data);
       })
       .catch((error) => {
         console.error("Error fetching data", error);
@@ -20,7 +21,14 @@ function HomePage() {
   return (
     <div>
       <p>HomePage</p>
-      {data}
+      {data.map((e) => (
+        <div className=" d-flex flex-row ">
+          {" "}
+          <p>{e.name}</p>
+          <p>{e.population}</p>
+          <p>{e.time}</p>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/pages/HomePage/IProps.ts
@@ -0,0 +1,7 @@
+export type censusData = {
+  id: number;
+  population: string;
+  time: number;
+  name: string;
+  geo: string;
+};
