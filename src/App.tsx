34c111208c7f96c65f99672a07127d8aef62971e
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2024-02-14 12:42:03 -0500

    adding auth routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import LoginPage from "pages/LoginPage/LoginPage.tsx";
 import HomePage from "pages/HomePage/HomePage";
 
 function App() {
+  const authToken = document.cookie.includes("authToken");
+
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/Login" Component={LoginPage} />
-          <Route path="/Dashboard" Component={HomePage} />
+          <Route path="/Login" element={<LoginPage />} />
+          <Route
+            path="/Dashboard"
+            element={authToken ? <HomePage /> : <Navigate to="/Login" />}
+          />
+          <Route
+            path="/"
+            element={
+              authToken ? (
+                <Navigate to="/Dashboard" />
+              ) : (
+                <Navigate to="/Login" />
+              )
+            }
+          />
         </Routes>
       </div>
     </Router>
