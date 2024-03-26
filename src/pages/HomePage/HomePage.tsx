d96ecfcca265133a4862983f471585dcb3ef1e77
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2024-03-26 14:29:36 -0400

    adding the map component , fecthing data

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,20 +1,41 @@
 import axios from "axios";
-import React, { useState, useEffect } from "react";
-import { censusData } from "./IProps";
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  DetailedHTMLProps,
+  InputHTMLAttributes,
+} from "react";
+import { censusData, Icountry } from "./IProps";
 import { useNavigate } from "react-router";
-import { Button } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
+import { Map } from "../../components/Map";
+import { countryMapData } from "../../components/data";
+import { handleLogOut } from "services/Logout";
 
 axios.defaults.withCredentials = true;
 
 const HomePage: React.FC = () => {
   const [data, setData] = useState<censusData[]>([]);
+  const [year, setYear] = useState(1800);
+  useRef<
+    DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+  >();
   const history = useNavigate();
   useEffect(() => {
     axios
-      .get("http://localhost:8080/")
+      .get("http://localhost:8080/census_data")
       .then((response) => {
-        console.log(response);
-        setData(response.data);
+        const formattedData: censusData[] = response.data.map(
+          (country: Icountry) => ({
+            id: country["query_text_values"].id,
+            geo: country["query_text_values"].geo,
+            name: country["query_text_values"].name,
+            time: country["query_text_values"].time,
+            Population: country["query_text_values"].Population,
+          })
+        );
+        setData(formattedData);
       })
       .catch((error) => {
         if (error.response && error.response.status === 401) {
@@ -27,16 +48,24 @@ const HomePage: React.FC = () => {
 
   return (
     <div>
-      <p>HomePage</p>
-      <Button>Log Out</Button>
-      {data?.map((e, i) => (
-        <div key={i} className="d-flex flex-row">
-          {" "}
-          <p>{e.name}</p>
-          <p>{e.population}</p>
-          <p>{e.time}</p>
+      <div className="mt-3 d-flex flex-row justify-content-around">
+        <div className="w-50">
+          <Form.Label>Year Selected: {year}</Form.Label>
+          <Form.Range
+            min={1800}
+            max={2050}
+            step={1}
+            value={year}
+            onChange={(e) => setYear(parseInt(e.currentTarget.value))}
+          />
         </div>
-      ))}
+        <Button onClick={() => handleLogOut()} className="mt-2 me-3">
+          Log Out
+        </Button>
+      </div>
+      <div className="d-flex flex-column align-items-center mt-5">
+        <Map data={countryMapData} width={1200} height={600} />
+      </div>
     </div>
   );
 };
